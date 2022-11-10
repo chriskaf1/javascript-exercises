cfbdea0aa1de9e13a654b9df473f201b615c2e0c
Christos Kafalis <chriskaf1@gmail.com>
2022-11-10 15:56:17 +0200

    Complete 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+// Sn = n(n+1)/2
 
+const sumToNumber = function(num) {
+    return num * (num+1) / 2;
+}
+
+const sumAll = function(numOne, numTwo) {
+    if (numTwo < 0 || numOne < 0 || typeof(numTwo) !== "number" || typeof(numOne) !== "number") {
+        return "ERROR";
+    }
+    if (numTwo > numOne) {
+        return sumToNumber(numTwo) - sumToNumber(numOne) + numOne;
+    } else {
+        return sumToNumber(numOne) - sumToNumber(numTwo) + numTwo;
+    } // We add numOne or numTwo respectively because it shouldn't be subtracted. We could also subtract sumToNumber(numX-1)
 };
 
 // Do not edit below this line
