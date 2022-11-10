750e6708bf945ee976cca1a6689e4b3dcbc0ae64
Christos Kafalis <chriskaf1@gmail.com>
2022-11-10 16:16:28 +0200

    Complete 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    return (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0));
+}; // Must be divisible by 4, and must at the same time be either divisible by 400 or not divisible by 100
 
 // Do not edit below this line
 module.exports = leapYears;
