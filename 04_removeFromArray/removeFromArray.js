09fe0ea99cbad9f1571e1b659c1fd79aec696982
Christos Kafalis <chriskaf1@gmail.com>
2022-11-10 15:17:59 +0200

    Complete 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    let toRemove = []; 
+    for (let i=1; i < arguments.length; i++) {
+        toRemove[i-1] = arguments[i];
+    } // create an array with all arguments
+    for (let arg of toRemove) { // loop for every argument that exists
+        let i = 0;
+        while (i < inputArray.length) { 
+            if (inputArray[i] === arg) {
+                inputArray.splice(i, 1); // Remove  the item if it matches the argument
+                // Syntax: splice(start, deleteCount, item1, item2, itemN) If you do not specify any elements, 
+                // splice() will only remove elements from the array.
+            } else { 
+                i++;
+            } // Only increase the counter if we're done with the whole array
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
